0e353c61696a023dbca93556934effa4c24d0828
MYX <it.anton22@gmail.com>
2023-12-21 22:07:22 +0300

    finished task

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -17,7 +17,7 @@ describe('test Cart', () => {
     cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
     cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
     cart.add(new Movie(1900, 'The Avengers', 2012, 'USA', 'Avengers Assemble!', 'Action', 137, 1000));
-    console.log(cart.items)
+
     expect(cart.items).toContainEqual({
       id: 1001,
       name: 'War and Piece',
@@ -34,7 +34,31 @@ describe('test Cart', () => {
     cart.add(buyable);
 
     expect(cart.items).toEqual([buyable]);
+  });
+
+  test('test method sum(), sumWithDiscount(), remove()', (done) => {
+    const cart = new Cart();
+
+    cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 25, 250));
+    cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 25));
+    cart.add(new Movie(1900, 'The Avengers', 2012, 'USA', 'Avengers Assemble!', 'Action', 137, 50));
+
+    expect(cart.sum()).toEqual(100);
+    expect(cart.sumWithDiscount(50)).toEqual(50);
+
+    const newCart = cart.remove(1001);
+
+    expect(cart.items).not.toContainEqual({
+      id: 1001,
+      name: 'War and Piece',
+      author: 'Leo Tolstoy',
+      price: 2000,
+      pages: 1225,
+    });
+
+    expect(cart.remove(10)).toBeUndefined();
+    done();
+  });
 });
-})
 
 
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,26 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+    // подсчет сумммы без скидки
+    sum(): number {
+        return this._items.reduce((acc, item) => acc + item.price, 0)
+    }
+    // подсчет суммы со скидкой
+    sumWithDiscount(value: number): number {
+        return this.sum() * value / 100;
+    }
+
+    // удаление из корзины по id
+    remove(id: number): Buyable | undefined {
+        const indexToRemove = this._items.findIndex(item => item.id === id);
+
+        if (indexToRemove !== -1) {
+            // Используем splice для удаления элемента по индексу
+            const [deletedItem] = this._items.splice(indexToRemove, 1);
+            return deletedItem;
+        }
+
+        return undefined; // Возвращаем undefined, если элемент не найден
+    }
 }
+
